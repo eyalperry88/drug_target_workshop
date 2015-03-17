4e0bb3459fa43061640819ef82ea53ef222a1496
Eyal Perry <eyal.perry88@gmail.com>
2015-03-17 15:11:30 +0200

    Parsing PPI data to edge weights + tests

---
+++ all_tests.py
@@ -0,0 +1,10 @@
+import os, sys
+
+print 'Running general test...'
+from tests import test
+print 'Done!'
+
+
+print 'Running parsing test...'
+from tests import parsing_test
+print 'Done!'
--- parsing_utils.py
+++ parsing_utils.py
@@ -2,6 +2,12 @@ from data_structures import *
 
 'reading and parsing data helpers'
 
+def loadGenes(filename, graph):
+    file = open(filename, 'r')
+    for line in file:
+        graph.addNode(line.strip())
+    file.close()
+    
 def loadExpressionData(filename, graph): #add option to do it by patient?
     file = open(filename, 'r')
     next(file) #first line has no data
@@ -19,4 +25,11 @@ def loadMutationData(filename, graph): #same...
         gene_data = line.split('\t')
         #filter by status?
         graph.nodes[gene_data[0]].mutation_type = gene_data[4] 
+    file.close()
+
+def loadPPIData(filename, graph):
+    file = open(filename, 'r')
+    for line in file:
+        gene_data = line.strip().split('\t')
+        graph.addEdge(gene_data[0].strip(), gene_data[1].strip(), float(gene_data[2]))
     file.close()
